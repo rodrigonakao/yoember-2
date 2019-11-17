3ffaf9b9a23e5219747a4910ee0e1243a21e8ea9
Rodrigo Nakao <rodrigo.nakao@xpi.com.br>
2019-11-17 16:57:48 -0300

    1 a 3

---
+++ app/controllers/index.js
@@ -0,0 +1,47 @@
+import Controller from '@ember/controller';
+import { computed, observer } from '@ember/object';
+import { match, not } from '@ember/object/computed';
+
+export default Controller.extend({
+
+  headerMessage: 'Comming Soon header',
+
+  //isDisabled: true,
+  // isDisabled: computed('emailAddress', function() {
+  //   return this.get('emailAddress') === '';
+  // }),
+  //isDisabled: computed.empty('emailAddress'),
+
+  isValid: match('emailAddress', /^.+@.+\..+$/),
+  isDisabled: not('isValid'),
+
+  emailAddress: '',
+
+  actions: {
+
+    saveInvitation: function() {
+
+      var _that = this;
+      var email = this.get('emailAddress');
+
+      var newInvitation = this.store.createRecord('invitation', {
+          email: email
+      });
+
+      newInvitation.save().then(function(response) {
+          _that.set('responseMessage', "Thank you! We saved your email address with the following id: " + response.get('id'));
+          _that.set('emailAddress', '');
+      });
+  }
+      
+  },
+
+  actualEmailAddress: computed('emailAddress', function() {
+    console.log('actualEmailAddress function is called: ', this.get('emailAddress'));
+  }),
+
+  emailAddressChanged: observer('emailAddress', function() {
+    console.log('observer is called', this.get('emailAddress'));
+  })
+
+});
