50ab16a93fea877a7e6638280f88ae6fe1e59799
Rodrigo Nakao <rodrigonakao@gmail.com>
2019-12-08 16:53:54 -0200

    Lesson 3 models

--- app/models/contact.js
+++ app/models/contact.js
@@ -1,6 +1,13 @@
 import DS from 'ember-data';
+import { match, gte, and, not } from '@ember/object/computed';
 
 export default DS.Model.extend({
   emailAddress: DS.attr('string'),
-  message: DS.attr('string')
+  message: DS.attr('string'),
+
+  isValidEmail: match('emailAddress', /^.+@.+\..+$/),
+  isMessageEnoughLong: gte('message.length', 5),
+
+  isValid: and('isValidEmail', 'isMessageEnoughLong'),
+  isNotValid: not('isValid')
 });
--- app/routes/contact.js
+++ app/routes/contact.js
@@ -1,4 +1,27 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+
+    model() {
+        return this.store.createRecord('contact');
+      }, 
+          
+      actions: {
+    
+        saveContact(newContactMessage) {            
+            newContactMessage.save().then(() => this.controller.set('responseMessage', true));
+        },
+    
+        willTransition() {
+    
+          let model = this.controller.get('model');
+    
+          if (model.get('isNew')) {
+            model.destroyRecord();
+          }
+
+          this.controller.set('responseMessage', false);
+        }
+     }
+
 });
