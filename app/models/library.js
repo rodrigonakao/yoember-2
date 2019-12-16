80c816437e5cd9a4e6f72e26b3848ff7c3fa1368
Rodrigo Nakao <rodrigonakao@gmail.com>
2019-12-16 00:42:55 -0300

    Lesson 6 - seeder - Faker

--- app/models/library.js
+++ app/models/library.js
@@ -1,10 +1,56 @@
-import DS from 'ember-data';
+// import DS from 'ember-data';
+// import { notEmpty } from '@ember/object/computed';
+
+// export default DS.Model.extend({
+//   name: DS.attr('string'),
+//   address: DS.attr('string'),
+//   phone: DS.attr('string'),
+
+//   isValid: notEmpty('name')
+// });
+
+
+// //Lesson 6
+// import DS from 'ember-data';
+// import { notEmpty } from '@ember/object/computed';
+
+// export default DS.Model.extend({
+
+//   name: DS.attr('string'),
+//   address: DS.attr('string'),
+//   phone: DS.attr('string'),
+
+//   books: DS.hasMany('book'),
+  
+//   isValid: notEmpty('name')
+// });
+
+
+//Lesson 6 - 2
 import { notEmpty } from '@ember/object/computed';
+import DS from 'ember-data';
+import Faker from 'faker';
 
 export default DS.Model.extend({
+
   name: DS.attr('string'),
   address: DS.attr('string'),
   phone: DS.attr('string'),
 
-  isValid: notEmpty('name')
+  books: DS.hasMany('book', { inverse: 'library', async: true }),
+
+  isValid: notEmpty('name'),
+
+  randomize() {
+    this.set('name', Faker.company.companyName() + ' Library');
+    this.set('address', this._fullAddress());
+    this.set('phone', Faker.phone.phoneNumber());
+
+    // If you would like to use in chain.
+    return this;
+  },
+
+  _fullAddress() {
+    return `${Faker.address.streetAddress()}, ${Faker.address.city()}`;
+  }
 });
--- app/routes/about.js
+++ app/routes/about.js
@@ -1,4 +1,39 @@
 import Route from '@ember/routing/route';
 
-export default Route.extend({
+ export default Route.extend({
 });
+
+
+// //Lesson 6
+// import Route from '@ember/routing/route';
+
+// export default Route.extend({
+
+//   init() {
+//     debugger;
+//   },
+
+//   beforeModel(transition) {
+//     debugger;
+//   },
+
+//   model(params, transition) {
+//     debugger;
+//   },
+
+//   afterModel(model, transition) {
+//     debugger;
+//   },
+
+//   activate() {
+//     debugger;
+//   },
+
+//   setupController(controller, model) {
+//     debugger;
+//   },
+
+//   renderTemplate(controller, model) {
+//     debugger;
+//   }
+// });
