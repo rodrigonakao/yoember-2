51d776d4b9189767141e057d65fbfc8b960f8227
Rodrigo Nakao <rodrigonakao@gmail.com>
2019-12-15 22:22:17 -0300

    Lesson 5 - nav-link-to component

---
+++ app/components/nav-link-to.js
@@ -0,0 +1,12 @@
+// import Component from '@ember/component';
+
+// export default Component.extend({
+// });
+
+
+//Lesson 5
+import LinkComponent from '@ember/routing/link-component';
+
+export default LinkComponent.extend({
+  tagName: 'li'
+});
--- app/routes/libraries/new.js
+++ app/routes/libraries/new.js
@@ -1,9 +1,50 @@
+// import Route from '@ember/routing/route';
+
+// export default Route.extend({
+
+//   model() {
+//     return this.store.createRecord('library');
+//   },
+
+//   actions: {
+
+//     saveLibrary(newLibrary) {
+//       newLibrary.save().then(() => this.transitionTo('libraries'));
+//     },
+
+//     willTransition() {
+//       // rollbackAttributes() removes the record from the store
+//       // if the model 'isNew'
+//       //this.controller.get('model').rollbackAttributes();
+
+//       let model = this.controller.get('model');
+
+//       if (model.get('isNew')) {
+//         model.destroyRecord();
+//       }
+//     }
+//   }
+// });
+
+
+//Lesson 5
 import Route from '@ember/routing/route';
 
 export default Route.extend({
 
   model() {
     return this.store.createRecord('library');
+  },
+
+  setupController(controller, model) {
+    this._super(controller, model);
+
+    controller.set('title', 'Create a new library');
+    controller.set('buttonLabel', 'Create');
+  },
+
+  renderTemplate() {
+    this.render('libraries/form');
   },
 
   actions: {
@@ -13,10 +54,6 @@ export default Route.extend({
     },
 
     willTransition() {
-      // rollbackAttributes() removes the record from the store
-      // if the model 'isNew'
-      //this.controller.get('model').rollbackAttributes();
-
       let model = this.controller.get('model');
 
       if (model.get('isNew')) {
